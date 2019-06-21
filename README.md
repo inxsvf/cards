970f66d8ebba237c559587a948cee9169d2f76cc
inxsvf <if3rreira@gmail.com>
2019-06-21 09:46:05 +0100

    get triggerdata from element. remove h-elements. remove outer margin. updated readme.

--- README.md
+++ README.md
@@ -2,35 +2,39 @@
 
 Ver [demo](https://https://users.fba.up.pt/~up200601296/ix/)
 
-## Alterar pergunta
+## Usage
 
-Nas linhas l.3-17 do ficheiro _triggerCard.js_, alterar o texto e os links:
+Na posição onde pretende inserir o _trigger card_, incluir os seguintes elementos, e alterar o conteúdo do _trigger_ pretendido. 
+```html
+	<div id="triggerCardRoot" 
+	data-trigger-question="Em qual destas danças, se perdem mais calorias por aula?"
+	data-trigger-description="Prepare-se para a praia e para as festas de verão de uma forma divertida."
+	data-trigger-correct-answer="2"
+	data-trigger-option1="Samba"
+	data-trigger-option2="Zumba"
+	data-trigger-option3="Pole-dance"
+	data-trigger-result-title="Os melhores tipos de dança para emagrecer e definir o corpo"
+	data-trigger-result-url="https://hintigo.com.br/danca-para-emagrecer/"
+	data-trigger-result-image-url="https://cdn.hintigo.com.br/uploads/2017/10/danca-para-emagrecer-425x318.jpg"></div>
 
-```javascript
-const triggerData = {
-	Question: "Em qual destas danças, se perdem mais calorias por aula?", 
-	QuestionDescription: "Prepare-se para a praia e para as festas de verão de uma forma divertida.", 
-	CorrectAnswerId: 2,
-	Answers: [
-		{ Id: 1, Value: "Samba" },
-		{ Id: 2, Value: "Zumba" },
-		{ Id: 3, Value: "Pole-dance" }
-	],
-	Result: {
-		Title: "Os melhores tipos de dança para emagrecer e definir o corpo",
-		Url: "https://hintigo.com.br/danca-para-emagrecer/",
-		Image: "https://cdn.hintigo.com.br/uploads/2017/10/danca-para-emagrecer-425x318.jpg"
-	}
-};
+	<script type="text/javascript" src="https://inxsvf.github.io/cards/triggerCard.js"></script>
 ```
 
-## Usar
+## Notas
 
-Na posiçao que é para aparecer, incluir:
+Nas seguintes situações, são referenciados os documentos aqui alojados:
+
+- Ao incluir o _trigger card_ : 
 ```html
-	<div id="shadowHost"></div>
+	<script type="text/javascript" src="https://inxsvf.github.io/cards/triggerCard.js"></script>
+```
 
-	<script type="text/javascript" src="triggerCard.js"></script>
+- Ao incluir os estilos, no ficheiro `triggerCard.js` l.19 : 
+```js
+	..
+	let cssFile = document.createElement('link');
+	cssFile.setAttribute('href', 'https://inxsvf.github.io/cards/main.css');
+	..
 ```
 
 
--- triggerCard.js
+++ triggerCard.js
@@ -1,34 +1,13 @@
-
-/* question definition */
-const triggerData = {
-	Question: "Em qual destas danças, se perdem mais calorias por aula?", 
-	QuestionDescription: "Prepare-se para a praia e para as festas de verão de uma forma divertida.", 
-	CorrectAnswerId: 2,
-	Answers: [
-		{ Id: 1, Value: "Samba" },
-		{ Id: 2, Value: "Zumba" },
-		{ Id: 3, Value: "Pole-dance" }
-	],
-	Result: {
-		Title: "Os melhores tipos de dança para emagrecer e definir o corpo",
-		Url: "https://hintigo.com.br/danca-para-emagrecer/",
-		Image: "https://cdn.hintigo.com.br/uploads/2017/10/danca-para-emagrecer-425x318.jpg"
-	}
-};
-/* END question definition */
-
-
 /* load structure and files */
-const mainTemplate = "<div class='triggerCard'> <header><hr/></header> <main> <div id='trigger' data-correctAnswer=''> <h1></h1> <h2></h2> <p class='secondary'>1323 pessoas já responderam</p> </div> <div id='optionsWrapper'></div> <div id='recommendation' hidden='true'> <h1>Para saber mais..</h1> </div> </main> <footer> <hr/> <p class='secondary right'><a href='#'>Termos e Condições</a></p> </footer> </div>";
+const mainTemplate = "<div class='triggerCard'> <header><hr/></header> <main> <div id='trigger' data-correct-answer=''> <div class='h1'></div> <div class='h2'></div> <p class='secondary'>1323 pessoas já responderam</p> </div> <div id='optionsWrapper'></div> <div id='recommendation' hidden='true'> <div class='h1'>Para saber mais..</div> </div> </main> <footer> <hr/> <p class='secondary right'><a href='#'>Termos e Condições</a></p> </footer> </div>";
 
 const optionTemplate = "<div id='option-{{id}}' class='choice' onclick='ShowAnswers(this)'><span class='text'>{{value}}</span></div>";
 
-const articleTemplate = "<a class='articleCard' href='{{url}}'><h1>{{title}}</h1><span class='thumbnail' style='background-image: url(\"{{img}}\");'></a>";
+const articleTemplate = "<a class='articleCard' href='{{url}}'><div class='h1'>{{title}}</div><span class='thumbnail' style='background-image: url(\"{{img}}\");'></a>";
 
-let host = document.querySelector('#shadowHost');
+let host = document.querySelector('#triggerCardRoot');
 let shadowRoot = host.attachShadow({mode: 'open'});
 shadowRoot.innerHTML = mainTemplate;
-
 
 let googleFonts = document.createElement('link');
 googleFonts.setAttribute('rel', 'stylesheet');
@@ -41,15 +20,31 @@ cssFile.setAttribute('href', 'https://inxsvf.github.io/cards/main.css');
 shadowRoot.querySelector('.triggerCard').appendChild(cssFile);
 
 /* load question */
+const triggerData = {
+	Question: host.dataset.triggerQuestion, 
+	QuestionDescription: host.dataset.triggerDescription, 
+	CorrectAnswerId: host.dataset.triggerCorrectAnswer,
+	Answers: [
+		{ Id: 1, Value: host.dataset.triggerOption1 },
+		{ Id: 2, Value: host.dataset.triggerOption2 },
+		{ Id: 3, Value: host.dataset.triggerOption3 }
+	],
+	Result: {
+		Title: host.dataset.triggerResultTitle,
+		Url: host.dataset.triggerResultUrl,
+		Image: host.dataset.triggerResultImageUrl
+	}
+};
+
 const options = triggerData.Answers.map( obj => optionTemplate.replace("{{id}}", obj.Id).replace("{{value}}", obj.Value) ).join('');
 
 const suggestion = articleTemplate.replace("{{url}}", triggerData.Result.Url)
 								  .replace("{{title}}", triggerData.Result.Title)
 								  .replace("{{img}}", triggerData.Result.Image);
 
-shadowRoot.querySelector('#trigger').dataset.correctanswer = 'option-' + triggerData.CorrectAnswerId;
-shadowRoot.querySelector('#trigger h1').textContent = triggerData.Question;
-shadowRoot.querySelector('#trigger h2').textContent = triggerData.QuestionDescription;
+shadowRoot.querySelector('#trigger').dataset.correctAnswer = 'option-' + triggerData.CorrectAnswerId;
+shadowRoot.querySelector('#trigger .h1').textContent = triggerData.Question;
+shadowRoot.querySelector('#trigger .h2').textContent = triggerData.QuestionDescription;
 shadowRoot.querySelector('#optionsWrapper').innerHTML = options;
 shadowRoot.querySelector('#recommendation').innerHTML += suggestion;
 
@@ -78,7 +73,7 @@ function DisableSelection() {
 }
 
 function ShowResults(choice) {
-	let correctOptionId = shadowRoot.querySelector('#trigger').dataset.correctanswer;
+	let correctOptionId = shadowRoot.querySelector('#trigger').dataset.correctAnswer;
 
 	let options = shadowRoot.querySelectorAll('[id^=option]');
 
